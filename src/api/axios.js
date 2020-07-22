3b57cb7a980eaab5b4ea3661defec4f38bb2c2f7
qqlcx5 <893917884@qq.com>
2020-07-23 00:31:18 +0800

    接口更新

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import qs from 'qs';
 
-axios.defaults.baseURL = "http://192.168.0.5:5000";
+axios.defaults.baseURL = "";
 axios.defaults.withCredentials = true;
 axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
 axios.defaults.transformRequest = data => qs.stringify(data);
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	lintOnSave: false,
 	publicPath: process.env.NODE_ENV === "production" ? "." : "/",
 	devServer: {
-		proxy: "https://news-at.zhihu.com/api/4"
+		// proxy: "https://news-at.zhihu.com/api/4"
+		proxy: "http://106.14.200.71:5500"
 	}
 };
